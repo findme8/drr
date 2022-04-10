e5ccd908bae059521c8f48234bb166b0b4077c12
findme8 <43626454+findme8@users.noreply.github.com>
2022-04-09 19:37:46 -0500

    Update dicom.py

--- dicom.py
+++ dicom.py
@@ -1,66 +1,3 @@
-# import h5py
-# import numpy as np
-# import os
-# import scipy.ndimage as ndimage
-#
-#
-# def get_dataset_from_txt_file(file_path):
-#
-#     with open(file_path, 'r') as f:
-#
-#         content = f.readlines()
-#
-#         return [i.strip() for i in content]
-#
-#
-# def load_file(file_path):
-#
-#     hdf5 = h5py.File(file_path, 'r')
-#
-#     ct_data = np.asarray(hdf5['ct'], dtype=np.float32) # 256, 256, 256
-#
-#     hdf5.close()
-#
-#     return ct_data
-#
-#
-# def get_image_path(root, index_name):
-#
-#     img_path = os.path.join(root, index_name, 'ct_xray_data.h5')
-#
-#     assert os.path.exists(img_path), 'Path do not exist: {}'.format(img_path)
-#
-#     return img_path
-#
-#
-# data_name = get_dataset_from_txt_file("C:/Users/Daisy/Desktop/Mdata/alldata.txt")
-#
-# for i in range(len(data_name)):
-#
-#     # Load original datasets
-#
-#     img_dir = get_image_path(r"C:\Users\Daisy\Desktop\Mdata\LIDC-HDF5-256", data_name[i])
-#
-#     ct_data = load_file(img_dir)
-#
-#     # folder name will store the datasets
-#
-#     folder_name = os.path.join(r'C:\Users\Daisy\Desktop\analysisdata\LIDC-IDRI', data_name[i])
-#
-#     os.mkdir(folder_name)
-#
-#     for j in range(64):
-#         # os.path.join('/content/test', "dataname_{0}.dcm".format(i))
-#
-#         img = sitk.GetImageFromArray(ct_data[j, :, :])
-#
-#         sitk.WriteImage(img, os.path.join(folder_name, "{0}.dcm".format(j)))
-
-
-
-
-
-
 import matplotlib.pyplot as plt
 import h5py
 import numpy as np
@@ -86,15 +23,7 @@ image3d = load_file(r"C:\Users\Daisy\Desktop\Mdata\LIDC-HDF5-256\LIDC-IDRI-0001.
 
 np.save('LIDC-IDRI-0001.20000101.3000566.1.npy', image3d)
 
-print(image3d.shape)
-
-test = np.load('LIDC-IDRI-0001.20000101.3000566.1.npy')
-
-print(test.shape)
-
-plt.imshow(test[30, :, :], cmap='gray')  # input X-ray
-
-plt.show()
+print(image3d.shape) # 256, 256, 256
 
 study = pydicom.uid.generate_uid()
 
